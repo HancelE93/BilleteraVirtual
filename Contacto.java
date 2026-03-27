22fc090c40358bdf080509f207cedc4dd0b85bc4
Coug4r <burneofrancisco2005@gmail.com>
2026-03-26 20:39:15 -0500

    Correcion Contacto

--- Contacto.java
+++ Contacto.java
@@ -1,28 +1,37 @@
-public class Contacto{
+public class Contacto {
     private String nombre;
     private String cuentaDestino;
     private String banco;
     private boolean favorito;
 
-    public Contacto(String nombre, String cuentaDestino, String banco){
+    public Contacto(String nombre, String cuentaDestino, String banco) {
         this.nombre = nombre;
         this.cuentaDestino = cuentaDestino;
         this.banco = banco;
         this.favorito = false;
     }
 
-    public void mostrarContacto(){
+    public void mostrarContacto() {
         String fav = "Sin Marcar";
-        if(favorito){fav = "⭐ Favorito"}
-        System.out.println("=== Contacto ===\nNombre: "+nombre+"\nCuenta: "+cuentaDestino+"\nBanco: "+banco+"\nFavorito: "+fav);
+        if (favorito) {
+            fav = "⭐ Favorito";
+        }
+        System.out.println("=== Contacto ===");
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Cuenta: " + cuentaDestino);
+        System.out.println("Banco: " + banco);
+        System.out.println("Favorito: " + fav);
     }
 
-    public void marcarFavorito(){
+    public void marcarFavorito() {
         favorito = true;
         System.out.println(nombre + " agregado a favoritos ⭐.");
     }
 
-    public void enviarDinero(double monto){
-       System.out.println("Transferencia enviada: \nDestinatario: "+nombre+"\nCuenta: "+cuentaDestino+"Monto: "monto);
+    public void enviarDinero(double monto) {
+        System.out.println("Transferencia enviada: ");
+        System.out.println("Destinatario: " + nombre);
+        System.out.println("Cuenta: " + cuentaDestino);
+        System.out.println("Monto: $" + monto);
     }
 }
