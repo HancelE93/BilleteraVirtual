0368d2284d45105703f44ec91e29eb2030fe3de4
Coug4r <burneofrancisco2005@gmail.com>
2026-03-26 20:38:13 -0500

    Add funtions Main

--- Main.java
+++ Main.java
@@ -11,11 +11,19 @@ public class Main {
  miBilletera.retirar(100.00);
  // ── Integrante 2: Transaccion ───────────────────────────────
  // Crear una instancia de Transaccion con datos reales.
- 
+ Transaccion pagoServicio = new Transaccion("Pago", 75.50, "Pago de servicio de internet");
  // Llamar a: mostrarDetalle(), aplicarComision() y marcarFallida().
+ pagoServicio.mostrarDetalle();
+ pagoServicio.aplicarComision(5);
+ pagoServicio.marcarFallida();
  // ── Integrante 3: Contacto ──────────────────────────────────
+ Contacto contacto1 = new Contacto("María López", "EC1122334455", "Banco Guayaquil");
  // Crear una instancia de Contacto con datos reales.
  // Llamar a: mostrarContacto(), marcarFavorito() y enviarDinero().
+ contacto1.mostrarContacto();
+ contacto1.marcarFavorito();
+ contacto1.enviarDinero(250.00);
+
  System.out.println();
  System.out.println("===== Fin del sistema =====");
  }
