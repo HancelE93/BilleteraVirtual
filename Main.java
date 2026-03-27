4369eb89dbe71d92edc5c96ac6dc97a3a255f175
Coug4r <burneofrancisco2005@gmail.com>
2026-03-26 20:35:08 -0500

    Add funtions

--- Main.java
+++ Main.java
@@ -6,8 +6,12 @@ public class Main {
  // Crear una instancia de Billetera con datos reales.
  Billetera miBilletera = new Billetera("David Pérez", "EC123456789", 150.00);
  // Llamar a: mostrarInfo(), depositar() y retirar().
+ miBilletera.mostrarInfo();
+ miBilletera.depositar(50.00);
+ miBilletera.retirar(100.00);
  // ── Integrante 2: Transaccion ───────────────────────────────
  // Crear una instancia de Transaccion con datos reales.
+ 
  // Llamar a: mostrarDetalle(), aplicarComision() y marcarFallida().
  // ── Integrante 3: Contacto ──────────────────────────────────
  // Crear una instancia de Contacto con datos reales.
